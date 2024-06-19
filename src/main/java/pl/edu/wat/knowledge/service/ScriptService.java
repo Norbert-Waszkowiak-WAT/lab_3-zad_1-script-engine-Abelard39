6ab1f1cee1cd9e96117e36fc35e8816d68e1cdf3
Abelard39 <64159164+Abelard39@users.noreply.github.com>
2024-06-19 21:24:10 +0000

    1,2,3,4?, 6

--- src/main/java/pl/edu/wat/knowledge/service/ScriptService.java
+++ src/main/java/pl/edu/wat/knowledge/service/ScriptService.java
@@ -6,19 +6,34 @@ import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.PolyglotException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pl.edu.wat.knowledge.repository.ArticleRepository;
-import pl.edu.wat.knowledge.repository.AuthorRepository;
+import pl.edu.wat.knowledge.repository.*;
 
 @Service
 @Slf4j
 public class ScriptService {
     private final ArticleRepository articleRepository;
     private final AuthorRepository authorRepository;
+    private final AffiliationRepository affiliationRepository;
+    private final PublisherRepository publisherRepository;
+    private final BookRepository bookRepository;
+    private final ChapterRepository chapterRepository;
+    private final JournalRepository journalRepository;
 
     @Autowired
-    public ScriptService(ArticleRepository articleRepository, AuthorRepository authorRepository) {
+    public ScriptService(ArticleRepository articleRepository,
+                        AuthorRepository authorRepository,
+                        AffiliationRepository affiliationRepository,
+                        PublisherRepository publisherRepository,
+                        BookRepository bookRepository,
+                        ChapterRepository chapterRepository,
+                        JournalRepository journalRepository) {
         this.articleRepository = articleRepository;
         this.authorRepository = authorRepository;
+        this.affiliationRepository = affiliationRepository;
+        this.publisherRepository = publisherRepository;
+        this.bookRepository = bookRepository;
+        this.chapterRepository = chapterRepository;
+        this.journalRepository = journalRepository;
     }
 
     public String exec(String script) {
@@ -28,6 +43,11 @@ public class ScriptService {
             var bindings = context.getBindings("js");
             bindings.putMember("articleRepository", articleRepository);
             bindings.putMember("authorRepository", authorRepository);
+            bindings.putMember("affiliationRepository", affiliationRepository);
+            bindings.putMember("publisherRepository", publisherRepository);
+            bindings.putMember("bookRepository", bookRepository);
+            bindings.putMember("chapterRepository", chapterRepository);
+            bindings.putMember("journalRepository", journalRepository);
             return context.eval("js", script).toString();
         } catch (PolyglotException e) {
             log.error("Error executing", e);
